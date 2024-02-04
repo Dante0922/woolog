a57f10e7a8d2fc0f6cfcf0d43111b7b83a3ed614
gw8413 <gw8413@gmail.com>
2024-02-04 17:07:36 +0900

    JWT cookie 인증 방식

--- src/main/java/com/woolog/config/AuthResolver.java
+++ src/main/java/com/woolog/config/AuthResolver.java
@@ -4,13 +4,17 @@ import com.woolog.config.data.UserSession;
 import com.woolog.domain.Session;
 import com.woolog.exception.Unauthorized;
 import com.woolog.repository.SessionRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@Slf4j
 @RequiredArgsConstructor
 public class AuthResolver implements HandlerMethodArgumentResolver {
 
@@ -23,10 +27,20 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        String accessToken = webRequest.getHeader("Authorization");
-        if(accessToken == null || accessToken.equals("")){
+        HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
+        if(servletRequest == null){
+            log.error("servletRequest null");
             throw new Unauthorized();
         }
+
+        Cookie[] cookies = servletRequest.getCookies();
+        if(cookies.length == 0){
+            log.error("cookie 없음");
+            throw new Unauthorized();
+        }
+
+        String accessToken = cookies[0].getValue();
+
 
         // DB 연계 확인 작업
         Session session = sessionRepository.findByAccessToken(accessToken).orElseThrow(Unauthorized::new);
